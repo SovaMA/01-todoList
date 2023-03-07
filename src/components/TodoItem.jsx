c9a72a04006cb332d38bea6ffdeb97ca411140b4
Mykhailo Sova <misha97sowa@gmail.com>
2023-03-07 10:31:17 +0200

    Add checkTodo on click <p>, Change style

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { useContext } from 'react'
-import DataContext from '../Context/Context'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from 'react';
+import DataContext from '../Context/Context';
 
-export default function TodoItem({id,title,completed}) {
-  const {deleteTodo,checkTodo} = useContext(DataContext);
+export default function TodoItem({ id, title, completed }) {
+  const { deleteTodo, checkTodo } = useContext(DataContext);
   return (
-    <li className="item">
-      <input className="item_checkbox" 
-             type="checkbox" 
-             onChange={()=>{checkTodo(id)}}
-             checked={completed}
+    <li className='item'>
+      <input
+        className='item_checkbox'
+        type='checkbox'
+        onChange={() => {
+          checkTodo(id);
+        }}
+        checked={completed}
       />
-      <p className={`item_title ${completed && 'line'}`} 
-         onDoubleClick={()=>{deleteTodo(id)}}
-      >      
+      <p
+        className={`item_title ${completed && 'line'}`}
+        onDoubleClick={() => {
+          deleteTodo(id);
+        }}
+        onClick={() => {
+          checkTodo(id);
+        }}>
         {title}
       </p>
-      <FontAwesomeIcon role='button' 
-                      tabIndex='0' 
-                      className="item_delete" 
-                      icon={faTrash}
-                      onClick={()=>{deleteTodo(id)}}
+      <FontAwesomeIcon
+        role='button'
+        tabIndex='0'
+        className='item_delete'
+        icon={faTrash}
+        onClick={() => {
+          deleteTodo(id);
+        }}
       />
     </li>
-  )
+  );
 }
